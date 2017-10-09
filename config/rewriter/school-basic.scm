bf1b8aae84f9bd62508e499054d941846858b851
Nathaniel Rudavsky-Brody <nathaniel.rudavsky@tenforce.com>
2017-10-09 18:31:40 +0200

    updated plugins

--- config/rewriter/school-basic.scm
+++ config/rewriter/school-basic.scm
@@ -5,7 +5,7 @@
 (define-namespace school "http://mu.semte.ch/vocabularies/school/")
 
 (define-constraint
-  'read/write
+  'read
   (lambda ()
     (let ((session (header 'mu-session-id)))
       (conc "CONSTRUCT {"
@@ -19,7 +19,27 @@
             "  VALUES (?graph ?type) { "
             "    (graphs:subjects school:Subject) "
             "    (graphs:classes school:Class) "
-            "    (graphs:people school:Person) "
+            "    (graphs:people <http://xmlns.com/foaf/0.1/Person>) "
+            "    (graphs:grades school:Grade) "
+            "  }"
+            "}"))))
+
+(define-constraint
+  'write
+  (lambda ()
+    (let ((session (header 'mu-session-id)))
+      (conc "CONSTRUCT {"
+            "  ?s ?p ?o."
+            "}"
+            "WHERE {"
+            "  GRAPH ?graph {"
+            "    ?s ?p ?o."
+            "    ?s rdf:type ?type "
+            "  }"
+            "  VALUES (?graph ?type) { "
+            "    (graphs:subjects school:Subject) "
+            "    (graphs:classes school:Class) "
+            "    (graphs:people <http://xmlns.com/foaf/0.1/Person>) "
             "    (graphs:grades school:Grade) "
             "  }"
             "}"))))
